c23514b6210de16b3114d10bd744e8e128340db3
endaulster <105703311+endaulster@users.noreply.github.com>
2024-05-10 13:13:37 -0300

    Added a basic design just for testing

--- dist/main.js
+++ dist/main.js
@@ -1,1 +1,31 @@
-console.log("hello word");
+// Getting the data
+
+async function fetchData(cityName) {
+  //API
+  const apiUrl = `https://api.weatherapi.com/v1/forecast.json?key=007e6da3a17d467294b71246240404&q=${cityName}&days=1&aqi=no&alerts=no`;
+
+  // Then
+  try {
+    const response = await fetch(apiUrl);
+
+    if (!response.ok) {
+      throw new Error("Network error");
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("There was a problem: ", error);
+  }
+}
+
+let myWeather = fetchData("Buenos Aires")
+  .then((data) => {
+    const sabroso = data.current.temp_c;
+    console.log(sabroso);
+  })
+  .catch((error) => {
+    console.error("There was a problem fetching the data: ", error);
+  });
+
+//////
